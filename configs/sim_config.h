8bd1d27a28ee552889b8c2f703dc702216f11ffc
Sysiphus CFD <sysiphus@pop-os.localdomain>
2024-02-01 10:44:17 -0500

    initial files and file system

---
+++ configs/sim_config.h
@@ -0,0 +1,80 @@
+#include "olb3D.h"
+#include "olb3D.hh"
+
+using namespace olb;
+using namespace olb::descriptors;
+using namespace olb::graphics;
+using olb::util::Randomizer;
+using olb::util::Timer;
+using namespace particles;
+using namespace particles::subgrid;
+using namespace particles::communication;
+using namespace particles::dynamics;
+using namespace particles::creators;
+using namespace particles::io;
+
+using T = FLOATING_POINT_TYPE;
+typedef D3Q19<> DESCRIPTOR;
+typedef SubgridParticle3D PARTICLETYPE;
+typedef BGKdynamics<T,DESCRIPTOR> BulkDynamics;
+
+#define BOUZIDI
+
+#ifndef M_PI
+#define M_PI 3.14159265358979323846
+#endif
+
+//Physical Settings
+const T tau = 0.51;
+const T rhoP = 998.2; // Physical Density (kg/m3)
+const T muP = 0.02;
+const T nuP = muP / rhoP; // Physical Nu
+const T nuL = (tau - nuP) / 3; // Lattice Nu (LU)
+
+
+// Sim Resolution Determination
+const int N = 30;
+const T charMinL =0.005; // m
+const T deltaX = charMinL / N; // LU
+const T deltaT = (nuL * deltaX * deltaX) / nuP; // s
+const T maxAllowableLatticeVel = 0.4;
+char stlFileName[] = "artery1.stl";
+
+// Sim Time Settings
+const T fluidMaxPhysT = T( 1 );     // max. fluid simulation time in s, SI unit
+const T particleMaxPhysT = T( 10 ); // max. particle simulation time in s, SI unit
+
+// Average Velocity Determination
+const T flowRate = 2. * 1.0e-6; // m3/s
+const T radInlet = 0.005; // m
+const T avgVel = flowRate / (M_PI * radInlet* radInlet); // m/s
+const T avgLVel = (avgVel*deltaT) / deltaX; // LU
+
+// Particle Settings
+std::size_t noOfParticles = 30000;   // total number of inserted particles
+const T radius = 7.5e-5;            // particles radius
+const T partRho = 998.2;
+const T particleInjectionX = 0.0002;
+const T injectionRadius = 2.0; // LU away from the wall
+const T injectionP = 0.1; // LU, length of the injection cylinder (must be cylinder because 3D, small to make it like a surface)
+
+//Set capture method:
+// materialCapture: based on material number
+// wallCapture:     based on more accurate stl description
+typedef enum {materialCapture, wallCapture} ParticleDynamicsSetup;
+const ParticleDynamicsSetup particleDynamicsSetup = materialCapture;
+
+// center of inflow and outflow regions [m]
+Vector<T, 3> inletCenter( 0.000, 0.000019, 0.000025 );
+Vector<T, 3> outletCenter( 0.020, -0.000386, 0.000322 );
+
+
+// normals of inflow and outflow regions
+Vector<T, 3> inletNormal(T(-1), T(0), T(0) );
+Vector<T, 3> outletNormal( T(1), T(0), T(0) );
+
+//writing data constants
+char vtkFileName[] = "rtdVal";
+char vtkParticleFileName[] = "particle";
+const T physVTKiter = 0.1;
+const T physStatiter = 0.001;
---
+++ pyHelpers/discrete_rtd.py
@@ -0,0 +1,37 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+num_particles = 30000
+deltaT = 0.001
+data_file = "success_3"
+
+with open(data_file,"r") as f:
+
+    lines = f.readlines()
+
+stats_line = []
+
+for line in lines:
+    line_split = line.split()
+
+    if line_split[0] == '[captureStatistics]' and len(line_split) == 4:
+        stats_line.append(int(line_split[3].split('=')[1]))
+
+e_t = np.zeros(shape=np.asarray(stats_line).shape)
+
+for i, line in enumerate(stats_line):
+    if i == 0:
+        summer = 0
+    else:
+        summer+= line
+        e_t[i] = ((line - stats_line[i-1])/(num_particles*deltaT))
+
+t = np.arange(0, deltaT*len(e_t), deltaT)
+
+print(f"Area under the E curve  -- > {np.sum(e_t*deltaT)}")
+print(f"Mean Residence Time  -- > {np.trapz(t*e_t, t, deltaT)}")
+print(f"Space time (V/Q)  -- > {1000000*0.02*(np.pi*(0.005**2)) / 1.985}")
+
+plt.plot(t, e_t)
+#plt.xlim((0,2))
+plt.savefig("test.png")   
